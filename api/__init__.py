36d52aad9b74efb3a6a140e5ae960edad735383a
debboutr <debboutr@gmail.com>
2022-07-28 23:43:06 -0700

    add CORS *

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,8 @@
+import os
+
 from flask import Flask
+from flask_cors import CORS
+
 from .endpoints import blueprint
 
 
@@ -7,8 +11,10 @@ def create_app(config_file="../instance/settings.cfg"):
     app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(blueprint)
     app.config.from_pyfile(config_file)
-    app.config['RESTX_MASK_SWAGGER'] = False
+    app.config["RESTX_MASK_SWAGGER"] = False
 
+    CORS(app, resources={r"/*": {"Access-Control-Allow-Origin": "*"}})
+    print(os.environ)
     from . import db
 
     db.init_app(app)
